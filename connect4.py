fce88fcc825c740f362d71fa3a492633928d822e
Harry Marshall <harryyymarshall@gmail.com>
2023-10-11 17:31:31 +1100

    added comments

--- connect4.py
+++ connect4.py
@@ -3,6 +3,7 @@ cols = 7
 
 board = [[' ' for _ in range(cols)] for _ in range(rows)]
 
+# TAKES INPUT AND CHECKS IT IS VALID BEFORE USING IT
 def validateColumn():
     while True:
         col_str = input("What column? (1 - 7) ")
@@ -11,18 +12,24 @@ def validateColumn():
             if 1 <= col <= 7:
                 print("\n")
                 return col - 1
-    
+
+# PRINT THE BOARD ITERATING THROUGH THE 2D ARRAY
 def printBoard():
     for row in board:
         print(row)
- 
+
+# DROP PLAYER PIECE FUNCTION TAKING THE CURRENT PLAYER VALUE AND THE COLUMN INPUT THEY HAVE MADE
 def dropPiece(board, col, player):
+    # begin from the bottom row and iterate through
     for i in range(rows-1, -1, -1):
+        # if the row and column is empty place the piece
         if board[i][col] == ' ':
             board[i][col] = player
             break
 
+# CHECKS THE BOARD FOR A WINNING CONDITION FROM WHAT PLAYERS TURN IT IS
 def checkWin(board, piece):
+    # check horz win
     for r in range(rows):
         for c in range(cols - 3):
             if (
@@ -32,7 +39,7 @@ def checkWin(board, piece):
                 board[r][c + 3] == piece
             ):
                 return True
-
+    # check vert win
     for r in range(rows - 3):
         for c in range(cols):
             if (
@@ -42,7 +49,7 @@ def checkWin(board, piece):
                 board[r + 3][c] == piece
             ):
                 return True
-            
+    # check positive dia win
     for r in range(rows - 3):
         for c in range(cols - 3):
             if (
@@ -52,7 +59,7 @@ def checkWin(board, piece):
                 board[r + 3][c + 3] == piece
             ):
                 return True
-
+    # check negative dia win
     for r in range(3, rows):
         for c in range(cols - 3):
             if (
@@ -65,15 +72,18 @@ def checkWin(board, piece):
 
     return False
 
-
+# intilise the first player 
 player = 'x'
 
 while True:
+    #print the empty board to begin and after every turn
     printBoard()
+    #take validate input and make dropCol equal to it
     dropCol = validateColumn()
     dropPiece(board, dropCol, player)
     if checkWin(board, player):
         printBoard()
         print('Player: ' + player + ' wins!!!')
         break
+    #change the player value before next turn and iteration of the loop
     player = '%' if player == 'x' else 'x'
